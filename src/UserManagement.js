d430b6c23a2486173efc3ad4f4912095b70ac60c
Ghassan Naja <gassnaja@gmail.com>
2026-02-05 00:04:19 -0500

    Completed database functionality, added test method for DB

--- src/UserManagement.js
+++ src/UserManagement.js
@@ -7,6 +7,12 @@ const dir = path.dirname(file_name);
 
 const data_path = path.join(dir, "Data", "user_data.json");
 
+const referenceUser = {
+    username : "TestUser",
+    password : "hello123",
+    diet : "likes food"
+}
+
 let users = []
 
 /// Loads the user dictionaries from the database
@@ -24,6 +30,7 @@ function loadUsers() {
         console.log("Error loading user files -", error);
         users = [];
     }
+    users.forEach(validateUser)
 }
 
 /// Saves the user-password pairs to the database
@@ -41,32 +48,35 @@ function saveUsers() {
 
 /// Returns the user with the specified username, returns an empty dict if user does not exist
 function getUser(username) {
-    for (let user in users) {
-        if (user.username == username) {
-            return user;
-        }
-    }
-    return {};
+    return users.find(user => user?.username === username);
 }
 
 /// Add a new user to the list of all users
 function addUser(user) {
-    for (let u in users) {
-        if (u.username == user.username) {
-            return;
-        }
-    }
+    if (users.some(u => u.username === user.username))
+        return;
+    validateUser(user)
     users.push(user);
 }
 
 /// Update the data of the user with the new info
 function updateUser(username, newInfo) {
+    users.forEach((user) => {
+        if (user.username === username) {
+            Object.assign(user, newInfo)
+        }
+    });
+}
 
+function removeUser(username, password) {
+    users = users.filter(
+        user => !(user.username === username && user.password === password)
+    );
 }
 
 /// Returns true if the username exists and the password is correct, false otherwise
 function authenticateUser(username, password) {
-
+    return users.some(user => user.username === username && user.password === password)
 }
 
 /// Creates a new user if the username doesn't already exist
@@ -76,20 +86,96 @@ function createNewUser(username, password) {
 
 /// Returns true if the username is already in the database
 function userAlreadyExists(username) {
-
+    return users.some(user => user.username === username);
 }
 
 /// Allows a user to change their password
 function changePassword(username, oldPassword, newPassword) {
+    users.forEach((user) => {
+        if (user.username === username && user.password === oldPassword) {
+            user.password = newPassword;
+        }
+    });
+}
 
+/// Adds an attribute to all users, by providing a new key and an initial value
+function addAttributeToUsers(key, initialValue) {
+    referenceUser[key] = initialValue;
+    users.forEach(user => {
+        if (!(key in user)) {
+            user[key] = initialValue;
+        }
+    })
+}
+
+/// Checks that the user has all the attributes needed, and adds missing attributes
+/// Uses referenceUser as reference
+function validateUser(user) {
+    for (const key of Object.keys(referenceUser)) {
+        if (!(key in user)) {
+            user[key] = referenceUser[key];
+        }
+    }
 }
 
 export function testUserDatabase() {
-    loadUsers();
+    const user1 = {
+        username : "user1",
+        password : "Hello1",
+        diet : "burgers"
+    }
+    const user2 = {
+        username : "user2",
+        password : "Hello2",
+        diet : "shrimp"
+    }
+    const user3 = {
+        username : "user3",
+        password : "Hello3"
+    }
+
+
+    users = [user1, user2]
+
+    console.log("User 1 has diet, expected true", "diet" in user1)
+    console.log("User 3 has diet, expected false", "diet" in user3)
+    addUser(user3)
+    console.log("User 3 has diet, expected true", "diet" in user3)
+    console.log("User3 diet :", user3.diet)
+    
+    console.log("Getting user2 and printing password (expected Hello2) :", getUser("user2").password)
+    console.log("Getting user4 (undefined) :", getUser("user4"))
+
+    updateUser("user3", {
+        password : "PP",
+        diet : "vegan"
+    })
+    console.log("Updating user3 (expected user3, PP, vegan)", user3.username, user3.password, user3.diet)
+
+    removeUser("user2", "Weewee");
+    console.log("Tried to remove user with wrong password, user still exists:", getUser("user2") !== undefined)
+
+    removeUser("user2", "Hello2");
+    console.log("Tried to remove user with correct password, user still exists:", getUser("user2") !== undefined)
+
+    console.log("trying to add user that already exists...")
     addUser({
-        username : "TestUser",
-        password : "hello123",
-        diet : "likes food"
-    });
-    saveUsers()
+        username : "user1"
+    })
+
+    console.log("User 1 already exists (expected true) :", userAlreadyExists("user1"))
+
+    changePassword("user3", "Ah!", "ABC")
+    console.log("Changing user 3 password with incorrect password (should be PP) :", user3.password)
+
+    changePassword("user3", "PP", "ABC")
+    console.log("Changing user 3 password (should be ABC) :", user3.password)
+
+    addAttributeToUsers("meat", "medium rare")
+    console.log("Adding attributes to all users (expected medium rare):", user1.meat)
+
+    console.log("readding user2...")
+    addUser(user2)
+
+    saveUsers();
 }
