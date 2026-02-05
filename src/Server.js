4c0b707955045b70846d985e3a1fcf657a7e57e1
Ghassan Naja <gassnaja@gmail.com>
2026-02-05 00:33:44 -0500

    Added requests to login as user, create new session, and end session

--- src/Server.js
+++ src/Server.js
@@ -1,23 +1,55 @@
-//Importing required modules
-
-
 import express from 'express';
 import session from 'express-session';
-import { testUserDatabase } from "./UserManagement.js";
-//import fs from 'fs';
+//import { testUserDatabase } from "./UserManagement.js";
+
+import { loadUsers, authenticateUser, saveUsers, getUserAttribute } from "./UserManagement.js";
 
 const app = express();
 const PORT = 3000;
+loadUsers();
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret : "soen" }));
+app.use(express.json());
 
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!authenticateUser(username, password)) {
+        return res.send({success : false});
+    }
+
+    req.session.username = username;
+    return res.send({success : true})
+});
+
+app.get("/user/:key", (req, res) => {
+    if (!req.session.username) {
+        return res.send(undefined)
+    }
+
+    const username = req.session.username;
+    res.send(getUserAttribute(req.params.key, username));
+});
+
+app.get("/logout", (req, res) => {
+    if (!req.session.username)
+        return res.send({success : false})
+
+    saveUsers();
+
+    req.session.destroy(error => {
+        if (error) {
+            return res.send({success : false});
+        }
+        return res.send({success : true});
+    });
+});
 
 app.listen(PORT, () => {
 
     console.log(`Listening on port ${PORT}`);
 
 });
-
-testUserDatabase()
