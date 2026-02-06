9e1c0950f7840c42cd6ffa386a342ee817508a3a
an1shmehra <mehraanishh@gmail.com>
2026-02-06 15:40:02 -0500

    Fixed the UI related Issue.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
+import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './Login'
 import Register from './Register'
 import ProfileView from './ProfileView'
+import ProfileEdit from './ProfileEdit'
+import ChangePassword from './ChangePassword'
+
+function ProfilePage() {
+  const [view, setView] = useState('view')
+  const storedUser = localStorage.getItem('user')
+  const username = storedUser ? JSON.parse(storedUser).username : 'Guest'
+
+  if (view === 'edit') {
+    return (
+      <ProfileEdit
+        username={username}
+        onCancel={() => setView('view')}
+        onSaveSuccess={() => setView('view')}
+      />
+    )
+  }
+
+  if (view === 'password') {
+    return (
+      <ChangePassword
+        onCancel={() => setView('view')}
+        onChangeSuccess={() => setView('view')}
+      />
+    )
+  }
+
+  return (
+    <ProfileView
+      username={username}
+      onEdit={() => setView('edit')}
+      onChangePassword={() => setView('password')}
+    />
+  )
+}
 
 function App() {
   return (
@@ -9,7 +45,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<ProfileView username="user" />} />
+        <Route path="/profile" element={<ProfilePage />} />
         <Route path="/" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
